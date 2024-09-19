fca5a29d10a08e73cd163202556229f836153987
Esteban Necuse <estebannecuse@gmail.com>
2024-09-19 16:01:46 -0300

    commit arreglos 1

--- src/Arreglos.java
+++ src/Arreglos.java
@@ -2,6 +2,9 @@ public class Arreglos {
     public static void main(String[] args) {
         int[] enteros;
         enteros = new int[5]; // entre corchetes indico la cantidad de elementos
-
+        enteros[0] = 21;
+        // sintaxis simplificada de arreglos 
+        int[] enterosSimplificados = {100,200,300,400,500};
+        System.out.println(enterosSimplificados[0]); // imprime 100
     }
 }
