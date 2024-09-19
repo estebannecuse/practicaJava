e68c6d4b514935fe1220f873138bb2720f6748d6
Esteban Necuse <estebannecuse@gmail.com>
2024-09-19 15:39:03 -0300

    primer commit

---
+++ src/Arreglos.java
@@ -0,0 +1,7 @@
+public class Arreglos {
+    public static void main(String[] args) {
+        int[] enteros;
+        enteros = new int[5]; // entre corchetes indico la cantidad de elementos
+
+    }
+}
