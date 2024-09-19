ee3f75687574596a549b3961e025decd04009e5e
Esteban Necuse <estebannecuse@gmail.com>
2024-09-19 16:21:33 -0300

    commit arreglos final

--- src/Arreglos.java
+++ src/Arreglos.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Arreglos {
     public static void main(String[] args) {
         int[] enteros;
@@ -6,5 +8,40 @@ public class Arreglos {
         // sintaxis simplificada de arreglos 
         int[] enterosSimplificados = {100,200,300,400,500};
         System.out.println(enterosSimplificados[0]); // imprime 100
+        for(int i =0;i<enterosSimplificados.length;i++){
+            System.out.println("El valor del indice" + i + "es: "+ enterosSimplificados[i]); // imprime cada elemento del arreglo
+        }
+
+        //introducir valores a un arreglo 
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Proporciona el largo del arreglo");
+        int largoArreglo = Integer.parseInt(consola.nextLine());
+        int[] arreglo = new int[largoArreglo];
+        for(int i = 0;i<arreglo.length;i++){
+            System.out.println("Proporciona enteros["+i+"] = ");
+            arreglo[i] = Integer.parseInt(consola.nextLine());
+        }
+        // imprimo los valores 
+        System.out.println("\n Impresion del arreglo");
+        for(int i = 0;i<arreglo.length;i++){
+            System.out.println("arreglo[" + i + "] = " + arreglo[i]);
+        }
+
+        //promedio calificaciones 
+
+        System.out.println("Cuantas calificaciones deseas agregar?");
+        int totalCalificaciones = Integer.parseInt(consola.nextLine());
+
+        int[] calificaciones = new int[totalCalificaciones];
+        for(int i = 0;i<calificaciones.length;i++){
+            System.out.println("Proporciona la calificacion [" + i + "]");
+            calificaciones[i] = Integer.parseInt(consola.nextLine());
+        }
+        int sumaCalificaciones = 0;
+        for(int i = 0;i<calificaciones.length;i++){
+            sumaCalificaciones += calificaciones[i];
+        }
+        double promedio = sumaCalificaciones / totalCalificaciones;
+        System.out.println("El promedio de las calificaciones es: " + promedio);
     }
 }
