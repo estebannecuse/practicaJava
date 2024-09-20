0664e3daa88a52eed229f118cbc091918d88813e
Esteban Necuse <estebannecuse@gmail.com>
2024-09-20 16:28:38 -0300

    commit matrices 1

---
+++ src/Matrices.java
@@ -0,0 +1,16 @@
+public class Matrices {
+    public static void main(String[] args) {
+        //defino como constante 
+        final int Renglones = 2;
+        final int Columnas = 3;
+        //creo un array bidimensional
+        int[][] matriz = new int[Renglones][Columnas];
+        // lleno la matriz
+        matriz[0][0] = 100;
+        matriz[0][1] = 200;
+        matriz[0][2] = 300;
+        matriz[1][0] = 400;
+        matriz[1][1] = 500;
+        matriz[1][2] = 600;
+    }
+}
